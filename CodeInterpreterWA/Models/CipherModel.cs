5e2fa012a3db052eaae983569fead008540b4209
tomas <tomasz_kondej@o2.pl>
2020-12-03 16:20:26 +0100

    przeniesienie klas Cipher i Converter do Modelu

---
+++ CodeInterpreterWA/Models/CipherModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeInterpreterWA.Models
+{
+    public class CipherModel
+    {
+        public int Id { get; set; }
+        public string NameOfCipher { get; set; }
+        public CipherModel(int id, string nameOfCipher)
+        {
+            Id = id;
+            NameOfCipher = nameOfCipher;
+        }
+
+    }
+
+   
+}
---
+++ CodeInterpreterWA/Models/ConverterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeInterpreterWA.Models
+{
+    public class ConverterModel
+    {
+        public int Id { get; set; }
+        public string NameOfCOnverter { get; set; }
+        public ConverterModel(int id, string nameOfConverter)
+        {
+            Id = id;
+            NameOfCOnverter = nameOfConverter;
+        }
+    }
+}
